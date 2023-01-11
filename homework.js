07cde1e0ea23177ba68c2a76bde798cf5913b27c
aniobiobinna85@yahoo.com <aniobiobinna85@yahoo.com>
2023-01-11 15:09:36 +0100

    added comments to code

--- homework.js
+++ homework.js
@@ -1,9 +1,16 @@
 let n = 3;      //variable for counting the number of loops
-let string = "";
-let i;
-for (i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += "*";
+
+let string = "";        //variable for called string for holding an empty string... why?
+
+let i;                          //variable called i acts as the counter
+
+for (i = 1; i <= n; i++) {              //an external 'for' loop with a condition that is a counter set to 3 because n = 3
+  
+  
+    for (let j = 0; j < i; j++) {               //An internal 'for' loop with a condition that is a counter that runs only one time
+    
+    
+        string += "*";                                  //
   }
   string += "\n";
 }
