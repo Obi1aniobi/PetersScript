6c7ea75962f5338b126178b567292b12bb9b7cfd
aniobiobinna85@yahoo.com <aniobiobinna85@yahoo.com>
2023-01-11 11:48:27 +0100

    JavaScript homework

---
+++ homework.js
@@ -0,0 +1,23 @@
+let n = 3;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
+
+
+
+
+
+let b = 100;
+let bString = "";
+for (let i = 1; i <= b; i++) {
+  for (let j = 0; j < i; j++) {
+    bString += "*";
+  }
+  bString += "\n";
+}
+console.log(bString);
