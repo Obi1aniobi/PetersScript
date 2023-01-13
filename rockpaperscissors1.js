70b909433b2b91d5004fccb963efe56da1c30130
aniobiobinna85@yahoo.com <aniobiobinna85@yahoo.com>
2023-01-13 10:46:35 +0100

    saved some changes

--- rockpaperscissors1.js
+++ rockpaperscissors1.js
@@ -5,4 +5,13 @@ let userInput = ['rock', 'paper', 'scissors'];
     function getUserChoice(userInput){
         userInput = userInput.toLowerCase;
         console.log(userInput);
+
+        if (userInput === 'rock');
+        console.log(userInput);
+      } else {
+        console.log('Error!: please enter correct word');
       }
+      
+      //I need to re-write the code above...
+
+
